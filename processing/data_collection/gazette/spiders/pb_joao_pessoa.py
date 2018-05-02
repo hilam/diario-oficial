1011264ec446c9c0a6f093d946dd21f034e45957
Hildeberto <hildeberto@gmail.com>
2018-05-02 09:19:09 -0300

    Create pb_joao_pessoa.py

---
+++ processing/data_collection/gazette/spiders/pb_joao_pessoa.py
@@ -0,0 +1,9 @@
+# João Pessoa - Semanário Oficial
+# http://www.joaopessoa.pb.gov.br/semanariooficial/ (scrap this)
+# http://www.joaopessoa.pb.gov.br/portal/wp-content/uploads/2018/04/2018_1629.pdf (pdf link example)
+# http://transparencia.joaopessoa.pb.gov.br/ (other sources)
+# 
+# Paraíba
+# http://paraiba.pb.gov.br/diario-oficial/ (scrap this)
+# http://static.paraiba.pb.gov.br/2018/04/Diario-Oficial-01-05-2018.pdf (pdf link example)
+#
